1468eb4667fa6f00da4bb4dd9726bd0373609e49
GeneralNel <mushfiqarashid@gmail.com>
2020-10-21 01:01:14 -0500

    Added charts

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-
 //A json object to contain the relationship of counties and what city they are in.
 let countyToCity = {}
+const xlabels = [];
 
 //Austin Counties
 countyToCity['Williamson'] = 'Austin';
@@ -34,11 +34,12 @@ dataPoints['Dallas'] = {};
 dataPoints['Houston'] = {};
 dataPoints['San Antonio'] = {};
 
+
 //fetchData : fetch the covid data from the dataset.
 //The data set is from the NYT, they post the data they use in their public github.
 //https://github.com/nytimes/covid-19-data
 async function fetchData(){
-
+    
     //d3 is the imported library used to handle the csv data format. It is imported in the head element of the html document.
     covidDataset = await d3.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv");
 }
@@ -48,7 +49,13 @@ async function fetchData(){
 function organizeData(){
     //Loop through each data entry we received from the data set and filter out the entries we don't care about.
     let previousMap = {};
+    let firstVal = true;
     covidDataset.forEach(function(data){
+        let date = new Date(data['date']); 
+        if (date.getMonth() < 8){
+            return; // skip this data entry if before May
+        }
+
         if (data['state'] !== 'Texas'){
             return; //Skip this data entry, we only care about Texas!
         }       
@@ -60,20 +67,99 @@ function organizeData(){
         if (county in previousMap){
             previous = parseInt(previousMap[county]);
         }
-        
+        if (firstVal){
+            
+        }
         let city = countyToCity[county];
         if (data['date'] in dataPoints[city]){
             dataPoints[city][data['date']] += Math.max(0,(parseInt(data['cases'])-previous));
         }else{            
             dataPoints[city][data['date']] = Math.max(0,(parseInt(data['cases'])-previous));
         }
+
         previousMap[county] = data['cases'];
     });
+
+    
 }
 
 //graphData : graph the data onto the web page using the chart.js library!
 function graphData(){
     console.log(dataPoints);
+    let y1 = Object.values(dataPoints['Austin']);
+    let x1 = Object.keys(dataPoints['Austin']);
+    const ctx = document.getElementById('austinChart');
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: x1,
+            datasets: [{ 
+                data: y1,
+                label: ["New Cases"],
+                borderColor: "#0066ff",
+                fill: false
+              }]
+          },
+          options: {
+          }
+    });
+    
+    const ctx2 = document.getElementById('dallasChart');
+    const y2 = Object.values(dataPoints['Dallas']);
+    const x2 = Object.keys(dataPoints['Dallas']);
+    let myChart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: x2,
+            datasets: [{ 
+                data: y2,
+                label: ["New Cases"],
+                borderColor: "#0066ff",
+                fill: false
+              }]
+          },
+          options: {
+          }
+    });
+    
+    
+    const ctx3 = document.getElementById('houstonChart');
+    const y3 = Object.values(dataPoints['Houston']);
+    const x3 = Object.keys(dataPoints['Houston']);
+    let myChart3 = new Chart(ctx3, {
+        type: 'line',
+        data: {
+            labels: x3,
+            datasets: [{ 
+                data: y3,
+                label: ["New Cases"],
+                borderColor: "#0066ff",
+                fill: false
+              }]
+          },
+          options: {
+          }
+    });
+
+        
+    const ctx4 = document.getElementById('antonioChart');
+    const y4 = Object.values(dataPoints['San Antonio']);
+    const x4 = Object.keys(dataPoints['San Antonio']);
+    let myChart4 = new Chart(ctx4, {
+        type: 'line',
+        data: {
+            labels: x4,
+            datasets: [{ 
+                data: y4,
+                label: ["New Cases"],
+                borderColor: "#0066ff",
+                fill: false
+              }]
+          },
+          options: {
+          }
+    });
+    
 }
 
 async function a(){
